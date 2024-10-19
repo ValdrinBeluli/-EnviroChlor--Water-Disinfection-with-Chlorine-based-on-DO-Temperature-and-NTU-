e8c758357be44feb83f42a9a2e28f6d3d3ca37b2
Valdrin Beluli <154743167+ValdrinBeluli@users.noreply.github.com>
2024-10-19 23:35:35 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='An engineering tool for measurements and analyzes the necessary chlorine level for disinfecting water, depending on measured values for DO, temperature, and NTU.',  
     long_description=open('README.md').read(), 
     long_description_content_type='text/markdown',  
-    url='https://github.com/ValdrinBeluli/EnviroChlor',  
+    url='https://github.com/ValdrinBeluli/DriniChlor',  
     packages=find_packages(),  
     classifiers=[
         'Programming Language :: Python :: 3',  
